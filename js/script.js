ddbd4387d01badf25e95e60fafb6f3ce572deeb0
Asger Søvsø <Asger.sko@gmail.com>
2016-08-02 23:17:00 +0200

    menuToggle moved to viewmodel

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
 var viewmodel = {
 	init: function() {
 		console.log('viewModel.init run')
-		//console.log(model.neighboorhood)
 		model.init();
 		viewmodel.getFoursquare(model.foursquareURL, 'food', 'images/icons/food.png', 4),
 		viewmodel.getFoursquare(model.foursquareURL, 'drinks', 'images/icons/drinks.png', 4),
 		viewmodel.getFoursquare(model.foursquareURL, 'coffee', 'images/icons/coffee.png', 4),
 		viewmodel.getFoursquare(model.foursquareURL, 'sights', 'images/icons/sights.png', 4)
 		console.log("adding markers")
-		//console.log(model.neighboorhoodLocation);
 	},
 
-	// get lat and lng from google
-	// skal kunne håndtere flere resultater
-	// getLatLng: function(location) {
-	// 	var latlngURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location;
-	// 	var result;
-	// 	$.getJSON(latlngURL, function (data) {
-	// 		var location = data.results[0].geometry.location;
-	// 		console.log('fetching location')
-	// 	}).done(function(e) {
-	// 		console.log('location fetched')
-	// 		return location
-	// 	}).error(function(e) {
-	// 		alert("Unable to fetch coordinates from google");
-	// 	});
-		//console.log(result)
-		//return result
-	//},
+	menuToggle: function(){
+		document.querySelector( "#nav-toggle" ).classList.toggle( "active" );
+		if (model.listOpen() == false) {
+			model.listOpen(true);
+		} else {
+			model.listOpen(false);
+		}
+	},
 
 	addMarkers: function() {
 		for (var i = 0; i < model.places().length; i++) {
@@ -38,7 +27,7 @@ var viewmodel = {
 	addMarker: function(place) {
 		view.markers.push(new google.maps.Marker({
 			position: place.location,
-			map: map,
+			map: model.map,
 			animation: google.maps.Animation.DROP,
 			title: place.name,
 			icon: place.icon
@@ -60,7 +49,7 @@ var viewmodel = {
 			content: '<b>' + place.name + '</b>' + '<br>' + 'Rating: ' + place.rating + '<br>' + website + '<br><i id="review">"' + place.review + '"</i>',
 			maxWidth: 200
 		});
-		view.infowindow.open(map, view.markers[place.index]);
+		view.infowindow.open(model.map, view.markers[place.index]);
 		view.markers[place.index].setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function() {view.markers[place.index].setAnimation(null); }, 750);
 	},
@@ -70,7 +59,7 @@ var viewmodel = {
 	},
 
 	showMarker: function(i) {
-		view.markers[i].setMap(map);
+		view.markers[i].setMap(model.map);
 	},
 
 	listClick: function(data) {
@@ -159,37 +148,16 @@ var model = {
 		lng: 12.54854
 	},
 	places: ko.observableArray([]),
+	map: null,
+	initMap: function() {
+		console.log('initMapRun')
+		model.map = new google.maps.Map(document.getElementById('mapContainer'), {
+			center: {lat: model.neighboorhoodLocation.lat, lng: model.neighboorhoodLocation.lng},
+			zoom: 16,
+			disableDefaultUI: true
+		});
+	},
 	foursquareURL: 'https://api.foursquare.com/v2/venues/explore?client_id=0ICNLWRURL412ESDGRHE1QBZ4UIPCAWSNEHZHGHKI4ERTHSC&client_secret=HNBI1JXSBGIGHUBRJT1Q14RJ1X1WW4OD5ZNZSHJWOEGSJFZQ&v=20130815&ll=55.6638947,12.54254&limit=20&query='
 }
 
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-	this.classList.toggle( "active" );
-	if (model.listOpen() == false) {
-		model.listOpen(true);
-	} else {
-		model.listOpen(false);
-	}
-  });
-
-
-
-
-// var changeLoc = function(){
-//     console.log("change location");
-//     map.map.setCenter(new google.maps.LatLng( 45, 19 ) )
-// }
-//console.log(model.neighboorhoodLocation)
-//test = viewmodel.getLatLng(model.neighboorhood);
-
 viewmodel.init();
-
-var map;
-function initMap() {
-	console.log('initMapRun')
-	map = new google.maps.Map(document.getElementById('mapContainer'), {
-		center: {lat: model.neighboorhoodLocation.lat, lng: model.neighboorhoodLocation.lng},
-		zoom: 16,
-		disableDefaultUI: true
-	});
-}
