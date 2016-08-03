4370a377fb8ade21e3da4621b2ceeddcc1c57728
Asger Søvsø <Asger.sko@gmail.com>
2016-08-03 19:57:16 +0200

    Ready for handin

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 README
 
+Browse the streets of Vesterbro in Copenhagen. Places to grab a bite to eat, get coffee, get hammered in the bars or see some of the local sights have been loaded from Foursquare.
 
+Access a list with all the places by clicking hamburger icon. Clicking a name on the list or the corresponding marker on the map, will open a window with information about the place. Clicking the different categories will hide or show that category.
+
+-----------------------------------------------------------
 Marker icons is taken from https://mapicons.mapsmarker.com/
--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 var viewmodel = {
+	// Function for initialize foursquare data fetch
 	init: function() {
-		console.log('viewModel.init run')
-		model.init();
-		viewmodel.getFoursquare(model.foursquareURL, 'food', 'images/icons/food.png', 4),
-		viewmodel.getFoursquare(model.foursquareURL, 'drinks', 'images/icons/drinks.png', 4),
-		viewmodel.getFoursquare(model.foursquareURL, 'coffee', 'images/icons/coffee.png', 4),
-		viewmodel.getFoursquare(model.foursquareURL, 'sights', 'images/icons/sights.png', 4)
-		console.log("adding markers")
+		console.log('viewModel.init run');
+		ko.applyBindings(model);
+		viewmodel.getFoursquare(model.foursquareURL, 'food', 'images/icons/food.png', 4);
+		viewmodel.getFoursquare(model.foursquareURL, 'drinks', 'images/icons/drinks.png', 4);
+		viewmodel.getFoursquare(model.foursquareURL, 'coffee', 'images/icons/coffee.png', 4);
+		viewmodel.getFoursquare(model.foursquareURL, 'sights', 'images/icons/sights.png', 4);
 	},
-
+	// Function for handling hamburger icon animation and opening and closing the list
 	menuToggle: function(){
 		document.querySelector( "#nav-toggle" ).classList.toggle( "active" );
-		if (model.listOpen() == false) {
+		if (model.listOpen() === false) {
 			model.listOpen(true);
 		} else {
 			model.listOpen(false);
 		}
 	},
-
+	// Function for adding markers for all places
 	addMarkers: function() {
 		for (var i = 0; i < model.places().length; i++) {
 			viewmodel.addMarker(model.places()[i]);
 		}
 	},
-
+	// Function for adding a marker to the map
 	addMarker: function(place) {
 		view.markers.push(new google.maps.Marker({
 			position: place.location,
@@ -36,15 +36,15 @@ var viewmodel = {
 			viewmodel.openInfoWindow(place);
 		});
 	},
-
+	// Function for creating info windows and animating markers
 	openInfoWindow: function(place) {
 		if (view.infowindow) {
 			view.infowindow.close();
-		};
+		}
 		var website = 'No website';
 		if (place.url) {
 			website = '<a href="' + place.url + '">Website</a>';
-		};
+		}
 		view.infowindow = new google.maps.InfoWindow({
 			content: '<b>' + place.name + '</b>' + '<br>' + 'Rating: ' + place.rating + '<br>' + website + '<br><i id="review">"' + place.review + '"</i>',
 			maxWidth: 200
@@ -53,20 +53,19 @@ var viewmodel = {
 		view.markers[place.index].setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function() {view.markers[place.index].setAnimation(null); }, 750);
 	},
-
+	// Function for hiding a single marker
 	hideMarker: function(i) {
 		view.markers[i].setMap(null);
 	},
-
+	// Function for showing a single marker
 	showMarker: function(i) {
 		view.markers[i].setMap(model.map);
 	},
-
+	// Function for opening a info window when a place in the list is clicked
 	listClick: function(data) {
 		viewmodel.openInfoWindow(data);
-
 	},
-
+	// Function for collapsing and expanding the list categories and hiding and showing the corresponding markers on the map
 	listCatClick: function(index) {
 		if (view.listExpand[index]()) {
 			view.listExpand[index](false);
@@ -84,9 +83,9 @@ var viewmodel = {
 			}
 		}
 	},
-
+	// Function for fetching data from foursquare
 	getFoursquare: function(foursquareURL, query, icon, numberOfRequests) {
-		var URL = foursquareURL + query
+		var URL = foursquareURL + query;
 		$.ajax({
 			url: URL,
 			dataType: "jsonp",
@@ -95,8 +94,6 @@ var viewmodel = {
 			},
 			complete: function(data) {
 				for (var i = 0; i < data.responseJSON.response.groups[0].items.length; i++){
-					//console.log(data.responseJSON.response.groups[0].items[i].venue.name);
-					//console.log(data.responseJSON.response.groups[0].items[i].tips[0].text)
 					var place = {
 						name: data.responseJSON.response.groups[0].items[i].venue.name,
 						location: {lat: data.responseJSON.response.groups[0].items[i].venue.location.lat, lng: data.responseJSON.response.groups[0].items[i].venue.location.lng},
@@ -109,48 +106,40 @@ var viewmodel = {
 						review: data.responseJSON.response.groups[0].items[i].tips[0].text
 					};
 					model.places.push(place);
-					// Check wether it's the last request
+					// Check whether it's the last request
 					if (i == data.responseJSON.response.groups[0].items.length - 1) {
 						model.foursquareCounter ++;
-					};
+					}
 					if (model.foursquareCounter == numberOfRequests) {
 						viewmodel.addMarkers();
 						model.foursquareCounter = 0;
 					}
-					//var markerLoc = {lat: data.responseJSON.response.groups[0].items[i].venue.location.lat, lng: data.responseJSON.response.groups[0].items[i].venue.location.lng};
-					//var markerTitle = data.responseJSON.response.groups[0].items[i].venue.name;
-					//viewmodel.addMarker(markerLoc, markerTitle, icon);
-					console.log("foursquare data fetched")
 				}
 			}
-		})
+		});
 	}
-}
+};
 
 var view = {
 	markers: [],
 	listExpand: [ko.observable(true),ko.observable(true),ko.observable(true),ko.observable(true)],
 	infowindow: null
-}
+};
 
 var model = {
-	init: function() {
-		console.log('model.init run');
-		ko.applyBindings(model);
-	},
 	categories: ["food", "drinks", "coffee", "sights"],
 	foursquareCounter: 0,
 	listOpen: ko.observable(false),
 	googleApiKey: 'AIzaSyCiUDq49n825eKvj7ecY7wW_Z3M0k3b-M4',
-	//neighboorhood: 'Vesterbro, København',
-	neighboorhoodLocation: { //55.6638947,12.54254    55.672308, 12.563953
+	neighboorhoodLocation: {
 		lat: 55.6699947,
 		lng: 12.54854
 	},
 	places: ko.observableArray([]),
 	map: null,
+	// Function for creating a new map in the mapContainer div
 	initMap: function() {
-		console.log('initMapRun')
+		console.log('initMapRun');
 		model.map = new google.maps.Map(document.getElementById('mapContainer'), {
 			center: {lat: model.neighboorhoodLocation.lat, lng: model.neighboorhoodLocation.lng},
 			zoom: 16,
@@ -158,6 +147,6 @@ var model = {
 		});
 	},
 	foursquareURL: 'https://api.foursquare.com/v2/venues/explore?client_id=0ICNLWRURL412ESDGRHE1QBZ4UIPCAWSNEHZHGHKI4ERTHSC&client_secret=HNBI1JXSBGIGHUBRJT1Q14RJ1X1WW4OD5ZNZSHJWOEGSJFZQ&v=20130815&ll=55.6638947,12.54254&limit=20&query='
-}
+};
 
 viewmodel.init();
